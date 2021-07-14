bb22d24225d35b27eaf97438c68bb9038487e6bf
rlarjsdn2313 <rlarjsdn2313@github.com>
2021-07-14 14:07:49 +0900

    chdckLogin.js complete

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
 
+const checkLogin = require('./lib/checkLogin');
+
+const data_path = './data/';
 const port = 3000;
 
 
 app.get('/', (req, res) => {
+    id = 2;
+    password = '12345';
+    checkLogin.checkLogin(`${id}@${password}`, data_path);
     res.send('hello');
 });
 
