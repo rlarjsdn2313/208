fa70059832e2387c56903979153e3e46115c322e
rlarjsdn2313 <rlarjsdn2313@github.com>
2021-07-15 12:06:28 +0900

    basic login show setting

--- 208/src/App.js
+++ 208/src/App.js
@@ -2,38 +2,46 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+import Login from './Components/login';
+
+
 function App() {
   const [check, setCheck] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchCheck = async () => {
-      try {
-        setError(null);
-        setCheck(null);
-        setLoading(true);
+  // useEffect(() => {
+  //   const fetchCheck = async () => {
+  //     try {
+  //       setError(null);
+  //       setCheck(null);
+  //       setLoading(true);
 
-        const res = await axios.get(
-          'http://localhost:5000/api/login/2@12345'
-        );
+  //       const res = await axios.get(
+  //         'http://localhost:5000/api/login/2@12345'
+  //       );
 
-        setCheck(res.data);
-      } catch(e) {
-        setError(e);
-      }
-      setLoading(false);
-    };
-    fetchCheck();
-  }, []);
+  //       setCheck(res.data);
+  //     } catch(e) {
+  //       setError(e);
+  //     }
+  //     setLoading(false);
+  //   };
+  //   fetchCheck();
+  // }, []);
 
-  if (loading) return <div className="App">Loading...</div>;
-  if (error) return <div className="App">Error!</div>;
-  if (check) {
-    return <div className="App">Done!</div>
-  } else {
-    return <div className="App">False!</div>
-  }
+  // if (loading) return <div className="App">Loading...</div>;
+  // if (error) return <div className="App">Error!</div>;
+  // if (check) {
+  //   return <div className="App">Done!</div>
+  // } else {
+  //   return <div className="App">False!</div>
+  // }
+  return (
+    <div className="App">
+      <Login setCheck={setCheck}></Login>
+    </div>
+  )
 }
 
 export default App;
