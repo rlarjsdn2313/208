d127bad5a566d143910f1744291ee31981b05d9a
rlarjsdn2313 <rlarjsdn2313@github.com>
2021-07-22 12:08:43 +0900

    write module edit

--- routes/write.js
+++ routes/write.js
@@ -25,11 +25,18 @@ router.post('/push', (req, res) => {
 
     let today = new Date();
 
+    var i = 0;
+    while (i < req.body.content.length) {
+        if (req.body.content[i] == '\n') {
+            req.body.content[i] = '<br>'
+        }
+        i++;
+    }
     fs.writeFileSync(`${constants.data_path}article/${dirs.length + 1}.json`,
 `{
     "writer": "${user['name']}",
     "date": [${today.getFullYear()}, ${today.getMonth() + 1}, ${today.getDate()}, ${today.getHours()}],
-    "content": "${req.body.content}"
+    "content": "${(req.body.content).replace(/\n/gi, '<br>').replace(/\r/g, "")}"
 }`
     );
 
